38b3560f0f57faf4ba7ce9530fa98c4fc00846f2
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-05-11 22:09:23 -0700

    containers

---
+++ containers.py
@@ -0,0 +1,17 @@
+# containers - constructs that hold collectiions of values
+
+# Dictionary - similar to a JS object literal
+# mutable - values assigned to a key can be changed
+
+student = {
+    'name' : 'Alyssa',
+    'course' : 'SEIR',
+    'current_week': 7
+}
+
+print('student:', student)
+
+
+
+# List - similar to a 
+# Tuple
